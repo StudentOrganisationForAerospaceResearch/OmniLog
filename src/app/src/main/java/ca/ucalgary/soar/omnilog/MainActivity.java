14945a085a871a61c92c312de254ecab87c4dcb8
protocorbulo <adhamilt@ualberta.ca>
2017-06-15 01:01:43 -0600

    added button and text

--- src/app/src/main/java/ca/ucalgary/soar/omnilog/MainActivity.java
+++ src/app/src/main/java/ca/ucalgary/soar/omnilog/MainActivity.java
@@ -4,10 +4,16 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
     boolean logging;
+    Button button;
+    TextView text;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,7 +21,11 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
-        final Button button = (Button) findViewById(R.id.button);
+        button = (Button) findViewById(R.id.button);
+        text = (TextView) findViewById(R.id.textView);
+        stopLogging();
+
+
         button.setOnClickListener(new View.OnClickListener(){
             public void onClick(View v){
                if(!is_logging()){
@@ -36,10 +46,13 @@ public class MainActivity extends AppCompatActivity {
 
     public void startLogging(){
         logging=true;
+        text.setText("Logging");
+
     }
 
     public void stopLogging(){
         logging=false;
+        text.setText("Not Logging");
     }
 
 
