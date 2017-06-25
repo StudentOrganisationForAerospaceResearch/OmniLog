0b43b13440b87b9729c794c67559c67ada8d7017
CLundquist-1 <CLundquist-1@outlook.com>
2017-06-24 18:20:57 -0600

    Commented the DataGatherer

--- src/app/src/main/java/ca/ucalgary/soar/omnilog/DataGatherer.java
+++ src/app/src/main/java/ca/ucalgary/soar/omnilog/DataGatherer.java
@@ -26,11 +26,8 @@ public class DataGatherer implements SensorEventListener, LocationListener {
     private float[] data;
     private boolean logging;
     private static final int recordDelay = 10;
-    //private SensorEventListener
 
-    //                           accel, mag,       ,gyro,light, press,     , proxi, grav, LA,  rota, humid, temp,                      sigmo                    geo-rot,                                                 station,motion
-    //boolean[] sensorsAvailable = {true, true, false, true, true, true, false, false, true, true, true, true, true, false, false, false, false, false, false, true, false, false, false, false, false, false, false, false, false, false};
-    //                              1     2      3     4     5     6     7      8      9    10    11    12    13    14     15     16     17     18     19     20     21     22     23     24     25     26     27     28     29     30
+    //This is a preconstructed list denoting the sensors we actually care to listen for, flase represents those we don't such as heart rate etc.
     //                           accel, mag,       ,gyro,light, press,     , proxi, grav, LA,  rota, humid, temp,                      sigmo                geo-rot,                                                 station,motion
     boolean[] sensorsAvailable = {true, true, false, true, true, true, false, false, true, true, true, true, true, false, false, false, false, false, false, true,};
     //                              1     2      3     4     5     6     7      8      9    10    11    12    13    14     15     16     17     18     19     20
@@ -41,34 +38,56 @@ public class DataGatherer implements SensorEventListener, LocationListener {
     int[] sensorMap = new int[sensorsAvailable.length];
 
     public DataGatherer(SensorManager SM, LocationManager LM) {
+        //Stores the sensorManager and locationManager used by the device as passed down from the activity.
         sm = SM;
         lm = LM;
 
+        //Variable used to count the number of sensor "fields" actually available on the device
+        //Thus it only increments when the device actually has a the sensor we are looking for and will increment by the number of fields
+        //The sensor will return (ie, humidity only returns 1 value(the humidity percent), whereas the accelerometer returns three (x,y,z values)
         int count = 0;
         Sensor s;
+
+        //j is used as an adapter for sensorManager and sensorsAvaible. SensorManager's sensors start at 1, our array starts at index 0
         int j;
+
+        //Loop through the preconstructed list of sensors we wish to listen for
         for (int i = 0; i < sensorsAvailable.length; i++) {
+            //Once again j is used to map sensorsAvaiable to SensorManager's sensors which start at 1
             j = i + 1;
+            //If we do not care for the sensor then we skip it
             if (!sensorsAvailable[i])
                 continue;
 
+            //If we do care look for the default sensor on the device
             s = sm.getDefaultSensor(j);
+            //Check if it returns null, if it did the device does not have the sensors so set it to false in our list
+            //For example the nexus 5 we are using does not have a humidity sensors even though we would like to look for it (set as true in sensorsAvailable)
             if (s == null) {
                 sensorsAvailable[i] = false;
                 continue;
             }
+            //The count is increased by 3 for the sensors that return multiple values i.e.: accelerometer, rotation vector, magnetic field, etc.
             if (j == 1 || j == 2 || j == 3 || j == 4 || j == 9 || j == 10 || j == 11 || j == 14 || j == 15 || j == 16 || j == 20) {
                 count += 3;
             } else {
                 count++;
             }
         }
+        //Increase it by 3 because we would like to add the gps Latitude, Longitude, and Altitude
         count += 3;
+        //Construct a new array of Strings that will be used to form the "header" of the file (its format)
+        //It will list the names of the sensors fields in the order that we will be writing them
         sensors = new String[count];
+        //Start count back at 0, waste not want not (we'll use it for this loop)
         count = 0;
+        //Run through the new list of avaiable sensors (with the ones we don't have crossed out)
         for (int i = 0; i < sensorsAvailable.length; i++) {
+            //If we the sensor is not avaiable
             if (!sensorsAvailable[i])
                 continue;
+            //If it is we will append the field name's associated with the sensor to our sensor field list
+            //Count references how "full" our sensor field array is, and increments by the number of fields added to it
             switch (i) {
                 case 0:
                     sensorMap[i] = count;
@@ -194,6 +213,7 @@ public class DataGatherer implements SensorEventListener, LocationListener {
                     break;
             }
         }
+        //Attempt to add the gps fields if it is available
         try {
             if (lm.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
                 sensors[count] = "GPS_Latitude";
@@ -208,7 +228,10 @@ public class DataGatherer implements SensorEventListener, LocationListener {
             }
         } catch (SecurityException e) {
         }
+        //Construct the data array that will contain the data for each field we have
         data = new float[sensors.length];
+        //Runs some tests, you can check them out in the log, it will display the sensorsAvailable after crossing out the ones we don't have
+        //And the header of the written file (The sensor fields in the order we will right them)
         testSensorsAvailable();
         testSensorString(sensors);
         testMapTo();
@@ -216,10 +239,13 @@ public class DataGatherer implements SensorEventListener, LocationListener {
 
     public void startLogging() {
         Sensor s;
+        //Loop through the available sensors and start listenting for them at the fastest rate possible
         for (int i = 1; i <= sensorsAvailable.length; i++) {
             s = sm.getDefaultSensor(i);
             sm.registerListener(this, s, sm.SENSOR_DELAY_FASTEST);
         }
+
+        //Attempt to listen for the gps if it is available
         try {
             if (lm.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
                 lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
@@ -228,6 +254,7 @@ public class DataGatherer implements SensorEventListener, LocationListener {
             }
         } catch (SecurityException e) {
         }
+        //Set logging to true and open up a thread that will continuously write the sensor readings until logging is false (when logging is stopped)
         logging = true;
         record = new Record("Record");
         record.writeToFile(sensors);
